ce4c220cf12c9b60e252ce6a9d6123daab754edc
Kenneth <huizhouwunan@hotmail.com>
2023-08-20 20:53:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 VUNDLE=${HOME}/.vim/bundle/Vundle.vim
-
-
 # Clone or update Vundle.
 if [[ ! -d "${VUNDLE}" ]]; then
   git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${VUNDLE}"
 else
   git -C "${VUNDLE}" pull --quiet
 fi
+
+FZF=${HOME}/.fzf
+if [[ ! -d "${FZF}" ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF}"
+else
+  git -C "${VUNDLE}" pull --quiet
+fi
+~/.fzf/install
